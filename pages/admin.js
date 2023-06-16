679f3e80ce61c65cd4094ee6b6c51bc196871334
kurukururuu <apriansyah.fajar94@gmail.com>
2023-06-16 15:48:35 +0700

    fix: broken admin page in live site

--- pages/admin.js
+++ pages/admin.js
@@ -1,16 +1,32 @@
 import dynamic from 'next/dynamic'
+import React from 'react'
 const config = require('../public/admin/config.yml');
 
-const CMS = dynamic(
-  () =>
-    import('netlify-cms-app').then((cms) => {
-      cms.init({ config })
-    }),
-  { ssr: false, loading: () => <p>Loading...</p> }
-)
+// const CMS = dynamic(
+//   () =>
+//     import('netlify-cms-app').then((cms) => {
+//       console.log({cms: cms})
+//       cms.init({ config })
+//       // return cms()
+//     }),
+//   { ssr: false, loading: () => <p>Loading...</p> }
+// )
+
+// const AdminPage = () => {
+//   console.log(CMS)
+//   return CMS()
+// }
+
+// export default AdminPage
 
 const AdminPage = () => {
-  return <CMS />
+  import('netlify-cms-app').then((cms) => {
+    cms.init({ config })
+    return cms
+  })
 }
 
-export default AdminPage
+export default dynamic(() => Promise.resolve(AdminPage), {
+  ssr: false,
+  loading: () => <p>Loading...</p>
+})
